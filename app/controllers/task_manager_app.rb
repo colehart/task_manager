8956e4299072cac93f4cd466152d09f486952db0
Cole Hart <cole@colehart.com>
2018-05-07 13:52:08 -0600

    for class

---
+++ app/controllers/task_manager_app.rb
@@ -0,0 +1,47 @@
+require_relative '../models/task.rb'
+
+# Task Manager App class
+class TaskManagerApp < Sinatra::Base
+  set :root, File.expand_path('..', __dir__)
+  set :method_override, true # this allows us to use _method in the Edit form
+
+  get '/' do
+    erb :dashboard
+  end
+
+  get '/tasks' do
+    @tasks = Task.all
+    erb :index
+  end
+
+  get '/tasks/new' do
+    erb :new
+  end
+
+  post '/tasks' do
+    task = Task.new(params[:task])
+    task.save
+    redirect '/tasks'
+    # "<p>Params: #{params}</p> <p>Task params: #{params[:task]}</p>"
+  end
+
+  get '/tasks/:id' do
+    @task = Task.find(params[:id])
+    erb :show
+  end
+
+  get '/tasks/:id/edit' do
+    @task = Task.find(params[:id])
+    erb :edit
+  end
+
+  put '/tasks/:id' do |id|
+    Task.update(id.to_i, params[:task])
+    redirect "/tasks/#{id}"
+  end
+
+  delete '/tasks/:id' do |id|
+    Task.destroy(id.to_i)
+    redirect '/tasks'
+  end
+end
